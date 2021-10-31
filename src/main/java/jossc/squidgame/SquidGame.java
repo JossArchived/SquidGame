1118fb645af550aa77def337a6b2a8e3d9390b67
Josscoder <josscoder@hotmail.com>
2021-10-31 18:50:53 -0500

    feat: GameAPI lib updated to 1.1

--- src/main/java/jossc/squidgame/SquidGame.java
+++ src/main/java/jossc/squidgame/SquidGame.java
@@ -1,9 +1,7 @@
 package jossc.squidgame;
 
-import java.util.UUID;
+import cn.nukkit.Player;
 import jossc.game.Game;
-import jossc.squidgame.arena.Arena;
-import jossc.squidgame.listener.ProtectListener;
 import lombok.Getter;
 
 public class SquidGame extends Game {
@@ -11,25 +9,23 @@ public class SquidGame extends Game {
   @Getter
   private static SquidGame plugin;
 
-  @Getter
-  private Arena arena;
-
   @Override
   public void init() {
     plugin = this;
 
-    registerListener(new ProtectListener());
-
-    String uuid = UUID.randomUUID().toString().substring(0, 5);
-    String worldName = getServer().getDefaultLevel().getName();
-
-    arena = new Arena(this, uuid, worldName, 2, 12);
-    arena.registerMicroGames();
-    arena.enable();
+    setDefaultGameMode(Player.ADVENTURE);
   }
 
   @Override
-  public void close() {
-    arena.disable();
+  public String getGameName() {
+    return "Squid Game";
   }
+
+  @Override
+  public String getInstruction() {
+    return "";
+  }
+
+  @Override
+  public void close() {}
 }
