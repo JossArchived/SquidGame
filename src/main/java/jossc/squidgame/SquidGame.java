5ec60b8101fef9de6f1c479d1074f8f956c08515
Josscoder <josscoder@hotmail.com>
2021-12-27 20:57:47 -0500

    feat: last changes

--- src/main/java/jossc/squidgame/SquidGame.java
+++ src/main/java/jossc/squidgame/SquidGame.java
@@ -1,26 +1,5 @@
 package jossc.squidgame;
 
-import jossc.game.Game;
+public class SquidGame {
 
-public class SquidGame extends Game {
-
-  @Override
-  public String getGameName() {
-    return "";
-  }
-
-  @Override
-  public String getInstruction() {
-    return "";
-  }
-
-  @Override
-  public void init() {
-
-  }
-
-  @Override
-  public void close() {
-
-  }
 }
